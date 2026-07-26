e9d9437ad14b782c89c4a0101b26540c56547170
FanyaDs <2218068@scholar.itn.ac.id>
2026-07-26 23:39:40 +0800

    Initial commit - 2 Ruang Creative website

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  content: ['*.html/','./src/js/*.js',
+    './docs/*.html'],
+  darkMode: 'class', // or 'media' or false
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          50:  '#f9f9f9',
+          100: '#efefef',
+          200:  '#cccccc',
+          300:  '#b6b6b6',
+          400:  '#d9d9d9',
+          500:  '#7d7d7d',
+          600:  '#686465',
+          700:  '#4d4948',
+          800:  '#323232',
+          900:  '#1c1c1c'
+        },
+        primary: {
+          500:  '#E63946',
+          600:  '#d92c3a'
+        },
+        secondary: {
+          300:  '#8ECAE6',
+          500:  '#219EBC'
+        }
+      }
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+  },
+  variants: {
+    extend: {
+       backgroundOpacity: ['dark']
+    }
+  },
+  plugins: [],
+}
